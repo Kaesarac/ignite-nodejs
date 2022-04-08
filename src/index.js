509d051b32b87b96cb295b699fd0aef0c3324b0a
Augusto Cesar <augustocesar19silva@gmail.com>
2022-04-08 11:49:22 -0300

    added 'customer not found' verification

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const port = 3000;
 
+const customers = [];
+
 app.listen(port); //starts app
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("Hello caraio");
+  return res.send("Hello caraio").json;
+});
+app.post("/account", (req, res) => {
+  const { cpf } = req.body;
+  const { name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+app.get("/statement", (req, res) => {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found!" });
+  }
+  console.log(customer.cpf);
+  return res.json(customers.statement);
 });
 
 console.log(`Listening on PORT ${port}`);
